d7db62d6ffee78c309c433449e617fc9423f7720
janardhannallapati <janardhan.nallapati@gmail.com>
2016-01-11 13:59:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var app=express();
+var PORT =3000;
+var middleware=require('./middleware.js');
+app.use(middleware.logger);
+
+app.get('/',function(req,res){
+    	res.send("Hello express!");
+});
+app.get('/about',middleware.requireAuthentication,function(req,res){
+  res.send('This is about us');
+});
+app.use(express.static(__dirname+'/public'));
+app.listen(PORT,function(){
+	console.log('express server started at port '+PORT);
+});
